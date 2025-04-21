7647f164486ff4bbe580281ea7432aee486fb700
Obada-Rabah <zlfosomaobada@gmail.com>
2025-04-21 09:07:41 +0300

    edit imports in order model

--- models/orders.model.js
+++ models/orders.model.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/database';
+import sequelize from '../utils/db.js';
 
 const Order = sequelize.define('Order', {
     SellerId: {
