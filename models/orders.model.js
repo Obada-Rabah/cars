45c80548506ac5b850d89c895bf1287e170965e3
Obada-Rabah <zlfosomaobada@gmail.com>
2025-04-21 09:04:05 +0300

    add order model

---
+++ models/orders.model.js
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+const Order = sequelize.define('Order', {
+    SellerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    CustomerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    Type: {
+        type: DataTypes.ENUM('car', 'service'),
+        allowNull: false
+    },
+    ProductId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    CarModel: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Date:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.ENUM('pending', 'accepted', 'declined', 'completed'),
+        defaultValue: 'pending'
+    }
+}, { timestamps: true });
+
+module.exports = Order;
